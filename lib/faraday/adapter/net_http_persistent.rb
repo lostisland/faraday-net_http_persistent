63f209968b540a8517a399ddc97e1b1461a3f0eb
Olle Jonsson <olle.jonsson@auctionet.com>
2024-08-16 14:06:14 +0200

    Lint: standardrb --fix

--- lib/faraday/adapter/net_http_persistent.rb
+++ lib/faraday/adapter/net_http_persistent.rb
@@ -31,9 +31,9 @@ module Faraday
 
       NET_HTTP_EXCEPTIONS = exceptions.freeze
 
-      def initialize(app = nil, opts = {}, &block)
+      def initialize(app = nil, opts = {}, &)
         @ssl_cert_store = nil
-        super(app, opts, &block)
+        super
       end
 
       def call(env)
@@ -194,7 +194,7 @@ module Faraday
       end
 
       def http_set(http, attr, value)
-        http.send("#{attr}=", value) if http.send(attr) != value
+        http.send(:"#{attr}=", value) if http.send(attr) != value
       end
 
       def ssl_verify_mode(ssl)
